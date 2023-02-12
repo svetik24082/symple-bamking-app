a4ac2444fd237c7098f0a6777da2e81a9f05a739
Света <svetik24082@mail.ru>
2023-02-13 00:30:25 +0500

    1 коммит

---
+++ src/main/java/com/example/simplebamkingapp/SimpleBamkingAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplebamkingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBamkingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleBamkingAppApplication.class, args);
+    }
+
+}
